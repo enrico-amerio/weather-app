add5fc9cccee69f29fd8d18bd3a999d5c351d7c1
enrico-amerio <amerio.enrico95@gmail.com>
2024-07-12 12:05:28 +0200

    [add] API CALLS Five days forecast and air quality

--- src/App.vue
+++ src/App.vue
@@ -10,28 +10,47 @@
     },
     methods: {
       getCoordinates(){
-        console.log(this.store.city);
-        axios.get(this.store.getCityCoordinates + "q=" + this.store.city + "&appid=" + this.store.APIKey)
+        axios.get(this.store.getCityCoordinates + "q=" + this.store.searchBarValue + "&appid=" + this.store.APIKey)
         .then((result) => {
-          // console.log(result.data[0].lat);
-          this.getWeather(result)
+          this.store.selectedCity = result.data
+          this.getCurrentWeather()
+          this.getFiveDaysWeather()
+          this.getAirQuality()
         })
         .catch((error) => {
           console.log(error);
         })
       },
-      getWeather(result){
-        // console.log('getWeatherLOG', result)
-        axios.get(this.store.getCityWeather + "lat=" + result.data[0].lat + "&lon=" + result.data[0].lon + "&appid=" + this.store.APIKey)
+      getCurrentWeather(){
+        axios.get(this.store.getCityWeather + "/weather?units=metric&" + "lat=" + this.store.selectedCity[0].lat + "&lon=" + this.store.selectedCity[0].lon + "&appid=" + this.store.APIKey)
         .then((result) => {
-          this.store.selectedCity = result.data
-          console.log(">>>>>>>>>>>>", this.store.selectedCity);
-          
+          this.store.currentWeather = result.data
           
         })
         .catch((error) => {
           console.log(error);
         })
+      },
+      getFiveDaysWeather(){
+        axios.get(this.store.getCityWeather + "/forecast?units=metric&" + "lat=" + this.store.selectedCity[0].lat + "&lon=" + this.store.selectedCity[0].lon + "&appid=" + this.store.APIKey)
+        .then((result) => {
+          console.log('NEXT 5 DAYS>>>>',result);
+
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+      },
+      getAirQuality(){
+        axios.get(this.store.getCityWeather + "/air_pollution?" + "lat=" + this.store.selectedCity[0].lat + "&lon=" + this.store.selectedCity[0].lon + "&appid=" + this.store.APIKey)
+        .then((result) => {
+          console.log('AIR POLLUTION>>>>>>>>>>',result);
+
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+       
       }
     }
   }
@@ -39,19 +58,19 @@
 
 <template>
  <div class="container">
-  <form @submit.prevent="this.getCoordinates(this.store.city)">
-    <input type="text" v-model="this.store.city">
+  <form @submit.prevent="this.getCoordinates(this.store.searchBarValue)">
+    <input type="text" v-model="this.store.searchBarValue">
     <button class="btn btn-danger" type="submit">Invia</button>
   </form>
   
-  <div class="card" style="width: 18rem;" v-if="Object.keys(store.selectedCity).length > 0">
+  <div class="card" style="width: 18rem;" v-if="Object.keys(store.currentWeather).length > 0">
   <ul class="list-group list-group-flush">
-    <li class="list-group-item">City: {{ this.store.selectedCity.name }}</li>
-    <img :src="`http://openweathermap.org/img/wn/${this.store.selectedCity.weather[0].icon}@2x.png`" class="card-img-top" alt="...">
-    <li class="list-group-item">Weather Description: {{ this.store.selectedCity.weather[0].description }}</li>
-    <li class="list-group-item">Temperature: {{ this.store.selectedCity.main.temp }} °</li>
-    <li class="list-group-item"> Humidity: {{ this.store.selectedCity.main.humidity }} %</li>
-    <li class="list-group-item"> Wind Speed: {{ this.store.selectedCity.wind.speed }} km/h</li>
+    <li class="list-group-item">City: {{ this.store.currentWeather.name }}</li>
+    <img :src="`http://openweathermap.org/img/wn/${this.store.currentWeather.weather[0].icon}@2x.png`" class="card-img-top" alt="...">
+    <li class="list-group-item">Weather Description: {{ this.store.currentWeather.weather[0].description }}</li>
+    <li class="list-group-item">Temperature: {{ this.store.currentWeather.main.temp }} °</li>
+    <li class="list-group-item"> Humidity: {{ this.store.currentWeather.main.humidity }} %</li>
+    <li class="list-group-item"> Wind Speed: {{ this.store.currentWeather.wind.speed }} km/h</li>
   </ul>
 </div>
  </div>
--- src/data/store.js
+++ src/data/store.js
@@ -3,9 +3,10 @@ import { reactive } from 'vue'
 export const store = reactive({
   APIKey: import.meta.env.VITE_API_KEY,
   getCityCoordinates:"http://api.openweathermap.org/geo/1.0/direct?",
-  getCityWeather:"https://api.openweathermap.org/data/2.5/weather?units=metric&",
-  city: "",
+  getCityWeather:"https://api.openweathermap.org/data/2.5",
+  searchBarValue: "",
   lat:"",
   lon:"",
-  selectedCity:{},
+  selectedCity:[],
+  currentWeather:{},
 })
